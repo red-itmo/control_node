a2e794fb83d4965d8ccbdd258c78064398e19399
Aram Azbekyan <nindz9@gmail.com>
2017-01-18 15:59:12 +0300

    definded ControlNode class

---
+++ include/ControlNode.h
@@ -0,0 +1,24 @@
+/*                                                                
+ * JoystickTeleop.h
+ *
+ *  Created on: 18.01.2017
+ *      Email: nindz9@gmail.com
+ *          ITMO University
+ *          Robotics Engineering Department
+ */
+
+#ifndef CONTROLNODE_H_
+#define CONTROLNODE_H_
+
+class ControleNode
+{
+public:
+    ControlNode();
+    ~ControlNode();
+
+private:
+    ros::NodeHandle n;
+
+}
+
+#endif /* CONTROLNODE_H_ */
